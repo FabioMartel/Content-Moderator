50e4a09c0ef1efb2fcc23a5dc0d1ccc5a88be999
FabioMartel <fabio.martel@vallegrande.edu.pe>
2024-05-17 19:46:51 -0500

    Update

---
+++ ModerationText/src/main/java/pe/edu/vallegrande/app/controller/ModerationController.java
@@ -0,0 +1,51 @@
+package pe.edu.vallegrande.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.vallegrande.app.entity.ModerationResult;
+import pe.edu.vallegrande.app.service.ContentModeratorService;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/v1/moderation")
+public class ModerationController {
+
+    private final ContentModeratorService contentModeratorService;
+
+    @Autowired
+    public ModerationController(ContentModeratorService contentModeratorService) {
+        this.contentModeratorService = contentModeratorService;
+    }
+
+    @GetMapping()
+    public Flux<ModerationResult> getAll() {
+        return contentModeratorService.getAll();
+    }
+
+    @PostMapping("/save")
+    public Mono<ModerationResult> save(@RequestBody ModerationResult moderationResult) throws Exception {
+        return contentModeratorService.save(moderationResult);
+    }
+
+    @PostMapping("/crear")
+    public Mono<ModerationResult> crear(@RequestBody ModerationResult moderationResult) throws Exception {
+        return contentModeratorService.crear(moderationResult);
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<ModerationResult> update(@PathVariable Integer id, @RequestBody ModerationResult moderationResult) {
+        return contentModeratorService.update(id, moderationResult);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> delete(@PathVariable Integer id) {
+        return contentModeratorService.delete(id);
+    }
+
+    @GetMapping("/hello")
+    public Mono<String> helloWorld() {
+        return Mono.just("Hello, World!");
+    }
+
+}
---
+++ ModerationText/src/main/java/pe/edu/vallegrande/app/service/ContentModeratorService.java
@@ -0,0 +1,117 @@
+package pe.edu.vallegrande.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import pe.edu.vallegrande.app.entity.ModerationResult;
+import pe.edu.vallegrande.app.repository.ModerationResultRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.json.JSONObject;
+
+@Slf4j
+@Service
+public class ContentModeratorService {
+
+	private final ModerationResultRepository moderationResultRepository;
+
+
+	@Autowired
+
+    public ContentModeratorService(ModerationResultRepository moderationResultRepository) {
+        this.moderationResultRepository = moderationResultRepository;
+    }
+
+    public Mono<ModerationResult> crear(ModerationResult moderationResult) {
+        return moderationResultRepository.save(moderationResult);
+    }
+
+    public Flux<ModerationResult> getAll() {
+        log.info("Mostrando datos");
+        return moderationResultRepository.findAll();
+    }
+
+    public Mono<ModerationResult> save(ModerationResult moderationResult) throws Exception {
+        moderationResult.setConsultText("Is this a crap email abcdef@abcd.com, phone: 6657789887, IP: 255.255.255.255, 1 Microsoft Way, Redmond, WA 98052");
+    	//"Is this a crap email abcdef@abcd.com, phone: 6657789887, IP: 255.255.255.255, 1 Microsoft Way, Redmond, WA 98052"
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
+        @SuppressWarnings("deprecation")
+        RequestBody body = RequestBody.create(mediaType, moderationResult.getConsultText());
+
+        Request request = new Request.Builder()
+                .url("https://eastus.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessText/Screen?classify=True")
+                .method("POST", body)
+                .addHeader("Host", "eastus.api.cognitive.microsoft.com")
+                .addHeader("Content-Type", "text/plain")
+                .addHeader("Ocp-Apim-Subscription-Key", "80662798c6d1435699eace8404e593d1")
+                .build();
+        
+        try {
+            Response response = client.newCall(request).execute();
+            JSONObject jsonObject = new JSONObject(response.body().string());
+
+            moderationResult.setOriginalText(jsonObject.getString("OriginalText"));
+            moderationResult.setNormalizedText(jsonObject.getString("NormalizedText"));
+
+            log.info("Mostrando JSON " + jsonObject);
+            log.info("Mostrando ORIGINAL TEXT " + moderationResult.getOriginalText());
+            log.info("Mostrando NORMALIZED TEXT " + moderationResult.getNormalizedText());
+            log.info("Mostrando MODELO " + moderationResult.toString());
+
+            return moderationResultRepository.save(moderationResult);
+        } catch (Exception e) {
+            log.error("Error al procesar el texto moderado: " + e.getMessage());
+            return Mono.error(e);
+        }
+    }
+    
+    public Mono<ModerationResult> update(Integer id, ModerationResult moderationResult) {
+        return moderationResultRepository.findById(id)
+                .flatMap(existingResult -> {
+                    // Actualiza el texto original con el valor proporcionado
+                    existingResult.setOriginalText(moderationResult.getOriginalText());
+                    
+                    // Establece los valores de normalizedText y consultText mediante el servicio de moderación
+                    return updateModerationText(existingResult);
+                })
+                .onErrorResume(e -> {
+                    log.error("Error al actualizar el texto moderado: " + e.getMessage());
+                    return Mono.error(e);
+                });
+    }
+
+    private Mono<ModerationResult> updateModerationText(ModerationResult moderationResult) {
+        try {
+            // Normaliza el texto eliminando la censura
+            String normalizedText = moderationResult.getOriginalText().replaceAll("fucking", "");
+            
+            // Establece normalizedText sin censura y consultText con el texto original
+            moderationResult.setNormalizedText(normalizedText.trim());
+            moderationResult.setConsultText(moderationResult.getOriginalText());
+            
+            return moderationResultRepository.save(moderationResult);
+        } catch (Exception e) {
+            log.error("Error al actualizar el texto moderado: " + e.getMessage());
+            return Mono.error(e);
+        }
+    }
+
+    public Mono<Void> delete(Integer id) {
+        return moderationResultRepository.findById(id)
+                .flatMap(existingResult -> {
+                    existingResult.setStatus('I'); // Marcado como inactivo
+                    return moderationResultRepository.save(existingResult).then();
+                });
+    }
+    
+    public Flux<ModerationResult> findByActiveTrue() {
+        return moderationResultRepository.findByActiveTrue();
+    }
+
+}
